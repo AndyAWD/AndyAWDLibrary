4335d003721305e636f2a1d8bce6bf1820d71931
戴維廷 <andydai@hyper-light.net>
2019-05-13 15:21:55 +0800

    Merge branch 'feature/日期格式化工具' of https://github.com/AndyAWD/AndyAWDLibrary into feature/日期格式化工具

--- andyawdlibrary/src/main/java/tw/com/andyawd/andyawdlibrary/AWDConstants.java
+++ andyawdlibrary/src/main/java/tw/com/andyawd/andyawdlibrary/AWDConstants.java
@@ -83,4 +83,17 @@ public class AWDConstants {
     public static final String VIDEO_INTENT_TYPE = "video/*";
     public static final String AUDIO_INTENT_TYPE = "audio/*";
     public static final String TEXT_INTENT_TYPE = "text/*";
+
+    public static final String DATETIME_INPUT_01 = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATETIME_INPUT_02 = "yyyy-MM-dd hh:mm:ss";
+    public static final String DATETIME_INPUT_03 = "yyyy-MM-dd";
+    public static final String DATETIME_INPUT_04 = "hh:mm:ss";
+    public static final String DATETIME_INPUT_05 = "HH:mm:ss";
+
+    public static final String DATETIME_OUTPUT_COLON_01 = "hh:mm";
+    public static final String DATETIME_OUTPUT_COLON_02 = "HH:mm";
+    public static final String DATETIME_OUTPUT_COLON_03 = "aahh:mm";
+    public static final String DATETIME_OUTPUT_COLON_04 = "aaHH:mm";
+    //public static final String DATETIME_STYLE_04 = "yyyy/MM/dd";
+
 }
