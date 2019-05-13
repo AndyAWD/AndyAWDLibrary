435ef0c8e372f9d1bb75cb87912dcc4105d74a9c
戴維廷 <andydai@hyper-light.net>
2019-05-13 14:49:26 +0800

    修改方法名稱

--- andyawdlibrary/src/main/java/tw/com/andyawd/andyawdlibrary/AWDDateFormat.java
+++ andyawdlibrary/src/main/java/tw/com/andyawd/andyawdlibrary/AWDDateFormat.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 public class AWDDateFormat {
 
 
-    public String AWDDateFormat(String inputDate, String inputDatePattern, String youWantDatePattern, Locale locale) {
+    public String getDateFormat(String inputDate, String inputDatePattern, String youWantDatePattern, Locale locale) {
 
         //TODO
         SimpleDateFormat inputDateFormat = new SimpleDateFormat(inputDatePattern, locale);
