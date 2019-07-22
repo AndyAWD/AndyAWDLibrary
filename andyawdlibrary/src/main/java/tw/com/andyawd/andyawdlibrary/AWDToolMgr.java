ae910f8418cdf53f4a1c094b1ae707e0788a2f0c
戴維廷 <andydai@hyper-light.net>
2019-07-22 16:56:14 +0800

    feature: 整理ToolMgr

--- andyawdlibrary/src/main/java/tw/com/andyawd/andyawdlibrary/AWDToolMgr.java
+++ andyawdlibrary/src/main/java/tw/com/andyawd/andyawdlibrary/AWDToolMgr.java
@@ -15,8 +15,11 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.core.content.ContextCompat;
+
 import android.text.TextUtils;
 import android.util.Base64;
 import android.view.Gravity;
@@ -40,17 +43,6 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class AWDToolMgr {
 
-    private static Toast toast;
-
-    private PackageManager packageManager;
-    private List<PackageInfo> lstPackageManager = new ArrayList<>();
-    private List<String> lstPackageName = new ArrayList<>();
-    private Vibrator vibrator;
-
-    private AWDToolMgr() {
-
-    }
-
     public static AWDToolMgr getInstance() {
         return AWDToolMgrHolder.awdToolMgr;
     }
@@ -61,144 +53,124 @@ public class AWDToolMgr {
 
     /**
      * 圖檔轉Bas64
-     *
-     * @param pathName
-     * @param quality
-     * @return
      */
-    public String ImageTransformBase64Text(String pathName, int quality) {
+    public String imageTransformBase64(String pathName, int quality) {
         Bitmap bitmap = BitmapFactory.decodeFile(pathName);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
-        byte[] imageBytes = baos.toByteArray();
-        String base64Image = Base64.encodeToString(imageBytes, Base64.NO_WRAP);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, byteArrayOutputStream);
 
-        return base64Image;
+        return Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.NO_WRAP);
     }
 
     /**
      * Bas64轉圖檔
      */
-    public Bitmap Base64TextTransformImage(String base64Text) {
+    public Bitmap base64TransformImage(String base64Text) {
         byte[] bytes = Base64.decode(base64Text, Base64.NO_WRAP);
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
 
     /**
      * 取得這隻手機已經安裝的檔案名稱
-     *
-     * @param context
-     * @return
      */
     public List<String> getPageNameList(Context context) {
-        packageManager = context.getPackageManager();
-        lstPackageManager = packageManager.getInstalledPackages(0);
+        List<PackageInfo> packageManagerList;
+        List<String> packageNameString = new ArrayList<>();
 
-        if (lstPackageManager != null) {
-            int i = 0;
-            for (PackageInfo packageInfo : lstPackageManager) {
-                lstPackageName.add(packageInfo.packageName);
-                i++;
-            }
-            context = null;
-            return lstPackageName;
-        } else {
-            lstPackageName.add("");
-            context = null;
-            return lstPackageName;
+        packageManagerList = context.getPackageManager().getInstalledPackages(0);
+
+        if (null == packageManagerList){
+            packageNameString.add(AWDConstants.EMPTY_STRING);
+            return packageNameString;
         }
+
+        for (PackageInfo info : packageManagerList) {
+            packageNameString.add(info.packageName);
+        }
+
+        context = null;
+        return packageNameString;
     }
 
     /**
      * TextView Icon上色
-     *
-     * @param context
-     * @param textView
-     * @param color
      */
     public void setTextViewDrawableColor(Context context, TextView textView, int color) {
         for (Drawable drawable : textView.getCompoundDrawables()) {
-            if (drawable != null) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    drawable.setColorFilter(new PorterDuffColorFilter(context.getColor(color), PorterDuff.Mode.SRC_IN));
-                }
+            if (drawable == null) {
+                return;
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                drawable.setColorFilter(new PorterDuffColorFilter(context.getColor(color), PorterDuff.Mode.SRC_IN));
             }
         }
 
         context = null;
     }
 
-
-
     /**
      * X 面骰模擬器
-     *
-     * @return
      */
-    public String DiceRandomSimulator(int diceCount) {
-        String strRandom = "";
+    public String getDiceRandomSimulator(int diceCount) {
         Random random = new Random();
-        strRandom = String.valueOf(random.nextInt(diceCount) + 1);
-        return strRandom;
+        return String.valueOf(random.nextInt(diceCount) + 1);
     }
 
     /**
      * 簡單震動，會給一個0.1秒，強度50的震動
-     *
-     * @param context
      */
     @SuppressLint("MissingPermission")
     public void setVibrator(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("Vibrator", MODE_PRIVATE);
-        boolean strVibratorSelect = sharedPreferences.getBoolean("Vibrator", false);
-        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
-        if (vibrator.hasVibrator()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vibrator.vibrate(VibrationEffect.createOneShot(100, 50));
-            } else {
-                vibrator.vibrate(100);
-            }
+        if (!vibrator.hasVibrator()) {
+            context = null;
+            return;
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(100, 50));
+        } else {
+            vibrator.vibrate(100);
+        }
+
         context = null;
     }
 
     /**
      * 自定震動
-     *
-     * @param context
-     * @param VibratorTimer     設定毫秒數
-     * @param VibratorAmplitude 設定震動強度0~255
      */
     @SuppressLint("MissingPermission")
-    public void setVibrator(Context context, int VibratorTimer, int VibratorAmplitude) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("Vibrator", MODE_PRIVATE);
-        boolean strVibratorSelect = sharedPreferences.getBoolean("Vibrator", false);
-        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    public void setVibrator(Context context, int vibratorTimer, int vibratorAmplitude) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
-        if (vibrator.hasVibrator()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vibrator.vibrate(VibrationEffect.createOneShot(VibratorTimer, VibratorAmplitude));
-            } else {
-                vibrator.vibrate(VibratorTimer);
-            }
+        if (!vibrator.hasVibrator()) {
+            context = null;
+            return;
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            if (vibratorAmplitude > 255) {
+                vibratorAmplitude = 255;
+            }
+
+            vibrator.vibrate(VibrationEffect.createOneShot(vibratorTimer, vibratorAmplitude));
+        } else {
+            vibrator.vibrate(vibratorTimer);
+        }
+
         context = null;
     }
 
     /**
      * 手機有沒有使用代理伺服器
-     * 有使用 true
-     * 沒有使用 false
-     * @return
      */
     public boolean isPhoneSetProxy() {
-        if (TextUtils.isEmpty(System.getProperty("http.proxyHost"))) {
-            if (TextUtils.isEmpty(System.getProperty("http.proxyPort"))) {
-                return false;
-            } else {
-                return true;
-            }
+        if (TextUtils.isEmpty(System.getProperty(AWDConstants.HTTP_PROXY_HOST)) ||
+                TextUtils.isEmpty(System.getProperty(AWDConstants.HTTP_PROXY_PORT))) {
+            return false;
         } else {
             return true;
         }
